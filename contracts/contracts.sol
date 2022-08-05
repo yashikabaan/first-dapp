b08b808f85f417ab2645173ddecc045b33dc000d
yashikabaan <yashikabaan096@gmail.com>
2022-08-06 01:21:25 +0530

    init commit

---
+++ contracts/contracts.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+contract Contract {
+
+    string private text;
+
+    constructor(string memory initialText) {
+        text = initialText;
+    }
+
+    // returning the current message
+    function speak() public view returns (string memory) {
+        return text;
+    }
+
+    // changing the message
+    function changeText(string memory newText) public {
+        text = newText;
+    }
+}
